ede1a4da6890970d691cf1a9583447a4e77e7cc5
druid2007 <66019562+druid2007@users.noreply.github.com>
2020-08-06 01:21:24 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const defaultAttributes = {
 
 var instance = new vidbg('.video-bg', {
     mp4: '/video/world.mp4', // URL or relative path to MP4 video
-    webm: '/video/worl1d.webm', // URL or relative path to webm video
+    webm: '/video/world.webm', // URL or relative path to webm video
     poster: '/video/poster.jpg', // URL or relative path to fallback image
     overlay: false, // Boolean to display the overlay or not
 }, defaultAttributes);
