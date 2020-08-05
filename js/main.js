72d3c673b8fc33ea1834aa41ca49324dabbdaf14
druid2007 <fedotov2007@yandex.ru>
2020-08-05 21:05:43 +0500

    lesson2

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const defaultAttributes = {
+    autoplay: true,
+    controls: false,
+    loop: true,
+    muted: true,
+    playsInline: true
+}
+
+var instance = new vidbg('.video-bg', {
+    mp4: '/video/world.mp4', // URL or relative path to MP4 video
+    webm: '/video/worl1d.webm', // URL or relative path to webm video
+    poster: '/video/poster.jpg', // URL or relative path to fallback image
+    overlay: false, // Boolean to display the overlay or not
+}, defaultAttributes);
+
+var rellax = new Rellax('.rocket');
